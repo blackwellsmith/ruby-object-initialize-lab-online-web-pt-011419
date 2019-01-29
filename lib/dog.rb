dd484f8c9a791be15c0439343a038f40879cc41d
blackwell <blackwellsmithiv@gmail.com>
2019-01-29 14:17:44 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,7 +3,13 @@ def initialize(name, breed = "Mutt")
     @breed = breed
     @name = name 
   end
-  
+   def name=(name)
+    @name = name
+  end
+ 
+  def name
+    @name
+  end
  
   def breed=(breed)
     @breed = breed
